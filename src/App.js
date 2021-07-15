e1cc46aac135e5f8ff0c8eaf8405c4b323f29cd9
ziyagurel <ziyagurel55@hotmail.com>
2021-07-16 00:11:32 +0300

    added styles for css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Admin from './components/AdminPanel/Admin';
 import Product from './components/Product/Product';
 
 import fish from './images/main-fish.png';
+import useStyles from './styles';
 
 const App = () => {
-
+    const classes = useStyles();
 
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align= "center">Suat'ın Yeri Balık Restaurant</Typography>
-                <img src={fish} alt="mycloud" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align= "center">Suat'ın Yeri Balık Restaurant</Typography>
+                <img className={classes.image} src={fish} alt="mycloud" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify ="space-between" alignItems="stretch" spacing={4}>
+                    <Grid className = {classes.mainContainer} container justify ="space-between" alignItems="stretch" spacing={4}>
                         <Grid item xs={12} sm={7}>
                             <Product/>
                         </Grid>
