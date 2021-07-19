1423354485a91e7e5131942455778f6eabef7b57
ziyagurel <ziyagurel55@hotmail.com>
2021-07-19 15:57:35 +0300

    created admin panel for admin component

--- src/components/AdminPanel/Admin.js
+++ src/components/AdminPanel/Admin.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { TextField, Button, Typography, Paper, Checkbox, FormControlLabel } from '@material-ui/core';
+import FileBase from 'react-file-base64';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
 
-const Form = () => {
+const Admin = () => {
     const classes = useStyles();
+    const [productData, setProductData] = useState({ title: '', contents:'', price: '', selectedImage: '', isShow: true,  });
+
+    const handleSubmit = (e) => {
+
+    }
+
+    const clear = () => {
+        setProductData({title: '', contents:'', price: '', selectedImage: '', isShow: false})
+    }
     
     return (
-        <h1>Form</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6"> Menü Ekle </Typography>
+                <TextField name ="title" variant ="outlined" label = "Ürün Adı" fullWidth value = {productData.title} onChange = {(e) => setProductData({ ... productData, title: e.target.value})} />
+                <TextField name ="contents" variant ="outlined" label = "Açıklaması" fullWidth value = {productData.contents} onChange = {(e) => setProductData({ ... productData, contents: e.target.value})} />
+                <TextField name ="price" variant ="outlined" label = "Tags" fullWidth value = {productData.price} onChange = {(e) => setProductData({ ... productData, price: e.target.value.split(',')})} />
+                <div className={classes.fileInput}>
+                    <FileBase type = "file" multiple = {false} onDone = {({base64}) => setProductData({ ... productData, selectedImage: base64})}/>
+                </div>
+                <FormControlLabel control={<Checkbox checked={productData.isShow} onChange={(e) => setProductData({ ... productData, isShow: e.target.checked})} name="antoine" />} label="Göster/Gizle"/>
+                <Button className = {classes.buttonSubmit} variant = "contained" color = "primary" size = "large" type = "submit" fullWidth>Gönder</Button>
+                <Button variant = "contained" color = "secondary" size = "small" onClick = {clear} fullWidth>Temizle</Button>
+            </form>
+        </Paper>
     );
 }
 
-export default Form;
+export default Admin;
