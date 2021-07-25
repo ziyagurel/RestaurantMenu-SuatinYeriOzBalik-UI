678c7c88deae9bd63aee5d310a262c3c888cc6c1
ziyagurel <ziyagurel55@hotmail.com>
2021-07-25 21:52:37 +0300

    updated css for admin panel

--- src/components/AdminPanel/Admin.js
+++ src/components/AdminPanel/Admin.js
@@ -45,9 +45,9 @@ const Admin = () => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6"> Menü Ekle </Typography>
-                <TextField name ="title" variant ="outlined" label = "Ürün Adı" fullWidth value = {productData.title} onChange = {(e) => setProductData({ ... productData, title: e.target.value})} />
-                <TextField name ="contents" variant ="outlined" label = "Açıklaması" fullWidth value = {productData.contents} onChange = {(e) => setProductData({ ... productData, contents: e.target.value})} />
-                <TextField name ="price" variant ="outlined" label = "Fiyat" fullWidth value = {productData.price} onChange = {(e) => setProductData({ ... productData, price: e.target.value})} />
+                <TextField name ="title" variant ="outlined" label = "Ürün Adı" fullWidth value = {productData.title} onChange = {(e) => setProductData({ ...productData, title: e.target.value})} />
+                <TextField name ="contents" variant ="outlined" label = "Açıklaması" fullWidth value = {productData.contents} onChange = {(e) => setProductData({ ...productData, contents: e.target.value})} />
+                <TextField name ="price" variant ="outlined" label = "Fiyat" fullWidth value = {productData.price} onChange = {(e) => setProductData({ ...productData, price: e.target.value})} />
                 <FormControl className={classes.formControl}>
                     <InputLabel id="categoryLabelId">Kategori</InputLabel>
                         <Select className = {classes.select} labelId="categoryLabelId" id="categorySelectId" open={open} onClose={handleClose} onOpen={handleOpen} value={productData.category} onChange={handleChange}>
@@ -59,9 +59,9 @@ const Admin = () => {
                         </Select>
                 </FormControl>
                 <div className={classes.fileInput}>
-                    <FileBase type = "file" multiple = {false} onDone = {({base64}) => setProductData({ ... productData, selectedImage: base64})}/>
+                    <FileBase type = "file" multiple = {false} onDone = {({base64}) => setProductData({ ...productData, selectedImage: base64})}/>
                 </div>
-                <FormControlLabel control={<Checkbox checked={productData.isShow} onChange={(e) => setProductData({ ... productData, isShow: e.target.checked})} name="antoine" />} label="Göster/Gizle"/>
+                <FormControlLabel control={<Checkbox checked={productData.isShow} onChange={(e) => setProductData({ ...productData, isShow: e.target.checked})} name="antoine" />} label="Göster/Gizle"/>
                 <Button className = {classes.buttonSubmit} variant = "contained" color = "primary" size = "large" type = "submit" fullWidth>Gönder</Button>
                 <Button variant = "contained" color = "secondary" size = "small" onClick = {clear} fullWidth>Temizle</Button>
             </form>
